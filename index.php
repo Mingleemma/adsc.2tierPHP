68e658051907261d4cc70eb3ec7fa638dbdd5604
Mingleemma <mingleemma09@gmail.com>
2022-11-09 17:00:34 +0000

    Landing Page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    if(isset);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple 2-Tier Application</title>
+</head>
+<body bgcolor="lavendar">
+    <center><h1>Welcome to the Simple 2-Tier Application</h1></center>
+    <center>
+        <form action="index.php" method="POST">
+            <div>
+                <input type="text" placeholder="Enter your name here"><br><br>
+                <input type="text" placeholder="Enter your email here"><br><br>
+                <button type="submit">submit</button>
+            </div>
+        </form>
+    </center>
+
+<?php
+
+?>
+</body>
+</html>
